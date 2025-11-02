9074553040a0c750b2f3938f38fc36faad286187
barannguzel <baran.guzel@barsan.com>
2025-11-02 20:50:56 +0300

    upd
    upd

--- src/app/components/counter-card/counter-card.ts
+++ src/app/components/counter-card/counter-card.ts
@@ -11,6 +11,5 @@ import { CommonModule } from '@angular/common';
 export class CounterCardComponent {
   @Input() title = 'Toplam Kullanıcı';
   @Input() value: number | string = 0;
-  @Input() icon: string =
-    '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=group" />';
+  @Input() icon = 'group';
 }
